7733503090783f58dd76d0434f2e9435e60c350d
jgoezl <jgoezl@unal.edu.co>
2026-04-28 17:08:34 -0500

    Create Notas.java

---
+++ ejercicio1/Notas.java
@@ -0,0 +1,46 @@
+package actividad3.pkg1;
+
+public class Notas {
+    double[] listaNotas;
+    
+    public Notas() {
+        listaNotas = new double[5];
+    } 
+    
+    double calcularPromedio() {
+        double suma = 0;
+        for(int i = 0; i < listaNotas.length; i++) {
+            suma += listaNotas[i];
+        }
+        return (suma / listaNotas.length);
+    }
+    
+    double calcularDesviacion() {
+        double prom = calcularPromedio();
+        double suma = 0;
+        for(int i=0; i < listaNotas.length; i++) {
+            suma += Math.pow(listaNotas[i] - prom, 2);
+        }
+        return Math.sqrt(suma/listaNotas.length);
+    }
+    
+    double calcularMenor() {
+        double menor = listaNotas[0];
+        for(int i=0; i < listaNotas.length; i++){
+            if (listaNotas[i] < menor){
+                menor = listaNotas[i];
+            }
+        }
+        return menor;
+    }
+    
+    double calcularMayor() {
+        double mayor = listaNotas[0];
+        for(int i=0; i < listaNotas.length; i++){
+            if (listaNotas[i] > mayor){
+                mayor = listaNotas[i];
+            }
+        }
+        return mayor;
+    }
+}
