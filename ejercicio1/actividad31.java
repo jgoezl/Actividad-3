9c6d391f3169bde9da2d3fe7bf192f2cdeb224e7
jgoezl <jgoezl@unal.edu.co>
2026-04-28 17:07:57 -0500

    Create actividad31.java

---
+++ ejercicio1/actividad31.java
@@ -0,0 +1,11 @@
+package actividad3.pkg1;
+
+
+public class Actividad31 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true); 
+    }
+    
+}
