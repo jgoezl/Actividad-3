cea8f36b729d947ae4a433d0e680b642fb3fecd4
jgoezl <jgoezl@unal.edu.co>
2026-05-19 13:48:32 -0500

    Create FiguraGeometrica.java

---
+++ ejercicio2/FiguraGeometrica.java
@@ -0,0 +1,19 @@
+package actividad3.pkg2;
+
+public class FiguraGeometrica {
+    private double volumen;
+    private double superficie;
+    public void setVolumen(double volumen){
+        this.volumen = volumen;
+    }
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    
+    public double getVolumen(){
+        return this.volumen;
+    }
+    public double getSuperficie(){
+        return this.superficie;
+    }
+}
